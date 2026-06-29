f17cc7cc17b825c8a23657d9ee9c893b2183cc61
pavankumar-569 <pavankumarcoder@gmail.com>
2026-06-29 14:29:28 +0530

    fix-routing-collision-login-dashboard

--- src/main.jsx
+++ src/main.jsx
@@ -66,7 +66,7 @@ const App = () => {
     return (<BrowserRouter>
       <Routes>
         {/* Auth Routes */}
-        <Route path="/" element={<AuthRoute><Login /></AuthRoute>}/>
+        <Route path="/login" element={<AuthRoute><Login /></AuthRoute>}/>
         <Route path="/register" element={<AuthRoute><Register /></AuthRoute>}/>
         <Route path="/forgot-password" element={<AuthRoute><ForgotPassword /></AuthRoute>}/>
 
